85ddb0c49ac3f16dcb1c2c7e4165e8c2e626ff61
Tanay Agarwal <tanay.mittal25@gmail.com>
2018-08-22 16:14:14 +0530

    Add files via upload

--- models/order.js
+++ models/order.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const deepPopulate = require('mongoose-deep-populate')(mongoose)
 const Schema = mongoose.Schema;
 
 const OrderSchema = new Schema({
@@ -10,5 +11,7 @@ const OrderSchema = new Schema({
     }],
     created: { type: Date, default: Date.now}
 });
+
+OrderSchema.plugin(deepPopulate);
 
 module.exports = mongoose.model('Order', OrderSchema);
